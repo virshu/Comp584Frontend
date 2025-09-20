bbba5f3a43db5aacd55824a7a4f2023e72ce6d5b
Alirie Gray <alirie.gray@gmail.com>
2025-09-19 17:35:07 -0700

    add weather table

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButton } from '@angular/material/button';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatToolbarModule,
+    MatIcon,
+    MatButton,
+    MatIconButton],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss'
 })
--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,16 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { MatTable } from '@angular/material/table';
+import { MatCellDef } from '@angular/material/table';
+import { MatHeaderRow } from '@angular/material/table';
+import { MatHeaderRowDef } from '@angular/material/table';
+import { MatRow } from '@angular/material/table';
+import { MatRowDef } from '@angular/material/table';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [
+    MatTable
+],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
 export class Weather {
+  forecasts: WeatherData[] = [];
+
   constructor(http: HttpClient) { // dependency injection
     http.get<WeatherData[]>('http://localhost:5145/WeatherForecast').subscribe(res => {
+      this.forecasts = res;
       console.log(res);
     })
   } 
