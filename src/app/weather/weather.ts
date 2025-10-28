a3c8c83e34035023b7e20b890b4e3bea93bf95af
virshu <felix@rabinovich.org>
2025-10-27 22:41:42 -0700

    switched to pipes

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,19 +1,29 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
-export class Weather {
-  forecasts: WeatherData[] = [];
+export class Weather implements OnInit {
+  //forecasts: WeatherData[] = [];
 
-  constructor(http: HttpClient) { // dependency injection
-    http.get<WeatherData[]>('http://localhost:5145/WeatherForecast').subscribe(res => {
-      this.forecasts = res;
-    })
+  fc$!: Observable<WeatherData[]>;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fc$ = this.http.get<WeatherData[]>('http://localhost:5145/WeatherForecast');
+    /*
+    this.weatherService.getWeatherForecast().subscribe({
+      next: result => this.forecasts = result,
+      error: error => console.error('There was an error!', error)
+    });
+    */
   } 
 }
